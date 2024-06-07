4337b2ac9d66c83727f8498a8498640c4ab3ae53
Levantine-1 <github@levantine.io>
2024-06-07 10:02:49 -0700

    require auth for a mice

--- src/main/java/com/levantine/datagateway/WebConfig.java
+++ src/main/java/com/levantine/datagateway/WebConfig.java
@@ -14,5 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor).addPathPatterns("/listTokens/**");
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/amIAGoodMouse/**");
     }
 }
